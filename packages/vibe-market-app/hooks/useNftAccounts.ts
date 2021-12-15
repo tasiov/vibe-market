f61f7d2c93d219338a88a97623baf0146f0e6775
Tasio Victoria <tasiovictoria@gmail.com>
2021-12-15 13:57:37 -0500

    Don't filter NFT accounts using mint

--- packages/vibe-market-app/hooks/useNftAccounts.ts
+++ packages/vibe-market-app/hooks/useNftAccounts.ts
@@ -3,8 +3,6 @@ import { useContext, useEffect, useMemo, useState } from "react"
 import { PublicKey } from "@solana/web3.js"
 import { HToken } from "../models"
 import { AnchorAccountCacheContext } from "../contexts/AnchorAccountsCacheProvider"
-import { useAccounts } from "./useAccounts"
-import { BN } from "@project-serum/anchor"
 
 export const useNftAccounts = (ownerPublicKey: PublicKey | undefined) => {
   const anchorAccountCache = useContext(AnchorAccountCacheContext)
@@ -24,44 +22,19 @@ export const useNftAccounts = (ownerPublicKey: PublicKey | undefined) => {
     })()
   }, [anchorAccountCache.isEnabled, ownerPublicKey?.toString()])
 
-  const mintAddresses = useMemo(() => {
-    return _.reduce(
-      tokenAccounts,
-      (accum: PublicKey[], tokenAccount) => {
-        if (tokenAccount.data.amount === 1) {
-          accum.push(new PublicKey(tokenAccount.data.mint))
-        }
-        return accum
-      },
-      []
-    )
-  }, [tokenAccounts])
-
-  const [mints] = useAccounts("hMint", mintAddresses, { useCache: true })
-
   return useMemo(() => {
-    if (
-      !tokenAccounts ||
-      _.isEmpty(tokenAccounts) ||
-      !mints ||
-      _.isEmpty(mints)
-    ) {
+    if (!tokenAccounts || _.isEmpty(tokenAccounts)) {
       return undefined
     }
     return _.reduce(
       tokenAccounts,
       (accum: Record<string, HToken.HToken>, tokenAccount) => {
-        const mint = mints[tokenAccount.data.mint]
-        if (
-          tokenAccount.data.amount === 1 &&
-          mint.data.decimals === 0 &&
-          mint.data.supply.eq(new BN(1))
-        ) {
+        if (tokenAccount.data.amount === 1) {
           accum[tokenAccount.publicKey.toString()] = tokenAccount
         }
         return accum
       },
       {}
     )
-  }, [tokenAccounts, mints])
+  }, [tokenAccounts])
 }
