c31723bc34f92bd3d1ecb4c10bb74c79093ef480
Tasio Victoria <tasiovictoria@gmail.com>
2021-12-15 15:24:14 -0500

    Overwrite decimals in token registry

--- packages/vibe-market-app/hooks/useTokenRegistry.ts
+++ packages/vibe-market-app/hooks/useTokenRegistry.ts
@@ -16,7 +16,13 @@ export const useTokenRegistry = () => {
       const tokenMap = _.reduce(
         tokens,
         (accum: Record<string, TokenInfo>, tokenInfo) => {
-          accum[tokenInfo.address] = tokenInfo
+          accum[tokenInfo.address] = { ...tokenInfo }
+          if (
+            tokenInfo.address === "8o66EVAf4u2Hr21m2tuRrPtEXFPLr8G8aL1ETStP8fDu"
+          ) {
+            // @ts-ignore - skip readonly declaration in this case
+            accum[tokenInfo.address].decimals = 9
+          }
           return accum
         },
         {}
